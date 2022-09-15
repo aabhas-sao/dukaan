70510dcda380f7a643770ba3c02d2f5bcff6e512
Aabhas Sao <aabhassao0@gmail.com>
2022-09-16 01:58:45 +0530

    chore: add mongodb connnection
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      'mongodb+srv://aabhas:NogameNolife@cluster0.f5h10mp.mongodb.net/?retryWrites=true&w=majority',
+      {
+        dbName: 'dukaan',
+      },
+    ),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
