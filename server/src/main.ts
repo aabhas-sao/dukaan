221a93969136e83dacae1311d816732995ddaa00
Aabhas Sao <aabhassao0@gmail.com>
2022-09-16 01:06:50 +0530

    chore: initial server setup
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

---
+++ server/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+}
+bootstrap();
