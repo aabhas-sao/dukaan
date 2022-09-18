ca47b4a6dc60c0a2b5120b8063c5c8f7a40a0ceb
Aabhas Sao <aabhassao0@gmail.com>
2022-09-18 16:21:09 +0530

    feat: add create order ui and add product modal
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

--- client/src/components/orders/trackOrder.js
+++ client/src/components/orders/trackOrder.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Text} from '@ui-kitten/components';
+import {SafeAreaView} from 'react-native';
 
 const TrackOrder = () => {
-  return <Text>TrackOrder</Text>;
+  return (
+    <SafeAreaView>
+      <Text>TrackOrder</Text>
+    </SafeAreaView>
+  );
 };
 
 export default TrackOrder;
